46d8273d0fcfb45756ed8703b2fc43d75a747b1d
Chris Dent <chris.dent@gmail.com>
2013-03-29 15:55:15 +0000

    map tags into the proxy tiddlers and date info fields
    flickr has two types of dates: date_taken and date_upload
    
    with two different formats...

--- flicktid/__init__.py
+++ flicktid/__init__.py
@@ -12,6 +12,8 @@ import requests
 import urllib
 
 from docopt import docopt
+from calendar import timegm
+from time import strptime
 
 HELP = """Usage: flicktid [-vh] --user=<user> --pass=<pass> <setid> <bag-uri>
 
@@ -24,9 +26,10 @@ photos in a flickr set identified by setid.
 """
 
 URL_FIELDS = ['url_t', 'url_s', 'url_m', 'url_o']
+EXTRAS = URL_FIELDS + ['date_upload', 'date_taken', 'tags']
 API_KEY = "f9af7135045d17357d881e9a123cf3cd"
 
-SET_URI = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=%s&photoset_id=%s&extras=url_t%%2Curl_o%%2Curl_s%%2Curl_m&format=json&nojsoncallback=1"
+SET_URI = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=%s&photoset_id=%s&extras=%s&format=json&nojsoncallback=1"
 
 
 def get_set(options):
@@ -34,7 +37,8 @@ def get_set(options):
     Get the flickr set data.
     """
     verbose = options['--verbose']
-    set_request_uri = SET_URI % (API_KEY, options['<setid>'])
+    set_request_uri = SET_URI % (API_KEY, options['<setid>'],
+            '%2c'.join(EXTRAS))
     if verbose:
         print('GET', set_request_uri, file=sys.stderr)
     response = requests.get(set_request_uri)
@@ -57,6 +61,10 @@ def put_tiddler(options, url_type, photo):
     target_url = options['<bag-uri>']
     photo_url = photo[url_type]
 
+    tags = photo['tags'].split(' ')
+    date_taken = to_epoch(photo['datetaken']) # YYYY-MM-DD HH:MM:SS
+    date_uploaded = photo['dateupload'] # epoch
+
     # use the title on the photo if it exists
     # XXX: this might be a bad thing because of collisions
     title = photo['title']
@@ -64,7 +72,12 @@ def put_tiddler(options, url_type, photo):
         title = photo['id']
 
     suffix = url_type.split('_', 1)[1]
-    json_data = {'fields': {'_canonical_uri': photo_url}}
+    json_data = {'fields': {
+        '_canonical_uri': photo_url,
+        'datetaken': date_taken,
+        'dateupload': date_uploaded
+        },
+        'tags': tags}
 
     target_url = target_url + '/' + encode_name(title) + '_%s' % suffix
     if verbose:
@@ -78,6 +91,14 @@ def put_tiddler(options, url_type, photo):
         print(response.status_code, file=sys.stderr)
         print(response.text, file=sys.stderr)
         sys.exit(1)
+
+
+def to_epoch(datestring):
+    """
+    Turn a datestring into an epoch time.
+    """
+    time_struct =strptime('2013-03-23 16:56:34', '%Y-%m-%d %H:%M:%S')
+    return timegm(time_struct)
 
 
 def encode_name(name):
